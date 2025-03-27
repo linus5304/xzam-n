084fbda894368dbb23f7f85bf19f3a36ed3154d3
linusbayere <linusbayere@gmail.com>
2025-03-27 02:54:13 -0400

    Add component configuration and UI elements
    - Add schadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-4xl font-bold">Hello World</h1>
+        <Button>Click me</Button>
       </div>
     </div>
   );
